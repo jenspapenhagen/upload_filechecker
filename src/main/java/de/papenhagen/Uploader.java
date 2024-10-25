a4e7dd722ea25f556871d59733b8fd590c957d10
jenspapenhagen <jens.papenhagen@web.de>
2024-10-25 20:41:13 +0200

    sending HTTP PUT request

--- src/main/java/de/papenhagen/Uploader.java
+++ src/main/java/de/papenhagen/Uploader.java
@@ -1,14 +1,39 @@
 package de.papenhagen;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.nio.file.Path;
+import java.util.logging.Logger;
 
 public class Uploader {
+
+    private static final Logger LOGGER = Logger.getLogger(Uploader.class.getName());
     //TODO:
     // upload to tika service
     // upload the response into the embedding ollama instant
     // upload the embedding string with all the meta data from the file into an vector DB
 
-    public void uploadToTika(final Path path){
+    public void uploadToTika(final Path path) {
+
+        String jsonBody = null;
+        try (final HttpClient client = HttpClient.newHttpClient()) {
+            final HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://localhost:9998/tika"))
+                    .PUT(HttpRequest.BodyPublishers.ofFile(path))
+                    .setHeader("Accept", "application/json")
+                    .build();
+
+            final HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+           jsonBody = response.body();
+        } catch (IOException | InterruptedException ex) {
+            LOGGER.severe("Exception on upload to TIKA" + ex.getLocalizedMessage());
+        }
+
+
 
     }
 
