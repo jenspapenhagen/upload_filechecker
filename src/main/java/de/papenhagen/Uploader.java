bb0cf144489e3f0fca29c9aff4d9d83d0ab3fce1
jenspapenhagen <jens.papenhagen@web.de>
2024-10-26 09:54:23 +0200

    adding last step of the todo list

--- src/main/java/de/papenhagen/Uploader.java
+++ src/main/java/de/papenhagen/Uploader.java
@@ -11,6 +11,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.UUID;
 import java.util.logging.Logger;
 
 import static java.util.Objects.isNull;
@@ -19,22 +20,21 @@ public class Uploader {
 
     private static final Logger LOGGER = Logger.getLogger(Uploader.class.getName());
 
-    static final String TIKA_URL = "http://beta.offenedaten.de:9998/tika";
-    //System.getProperty("tikaURL");
-
-    static final String OLLAMA_API_URL = "http://localhost:11434/api/embeddings";
-
-    //TODO:
-    // upload to tika service  - DONE
-    // upload the response into the embedding ollama instant
-    // upload the embedding string with all the meta data from the file into an vector DB
+    private static final String TIKA_URL = System.getProperty("tikaURL");
+    private static final String OLLAMA_API_URL = System.getProperty("ollamaURL");
+    private static final String VECTOR_URL = System.getProperty("vectorURL");
 
     public void uploadToTika(final Path path) {
         if (isNull(path)) {
             return;
         }
-        LOGGER.severe("path:" + path);
+        LOGGER.info("path:" + path);
+        final String fileName = path.getFileName().toString();
+        LOGGER.info("fileName:" + fileName);
 
+        // Step 1:
+        // upload the file to Apache Tika
+        // more infos: https://tika.apache.org/
         String textBody = null;
         try (final HttpClient client = HttpClient.newHttpClient()) {
             final HttpRequest request = HttpRequest.newBuilder()
@@ -50,14 +50,18 @@ public class Uploader {
 
         LOGGER.info(textBody);
 
-        // We use a "local" model running in a ollama instance
-        // As embedding model we use: https://www.nomic.ai/blog/posts/nomic-embed-text-v1
-        //        nomic-embed-text is a large context length text encoder that surpasses OpenAI text-embedding-ada-002
-        //        and text-embedding-3-small performance on short and long context tasks.
+        // Step 2:
+        // upload text from the file to an embedding model
 
-        //maybe later we will use a 1b or 3b embedding model to save some RAM and CPU/GPU
+        // We use "local" model running on an ollama instance
+        // As an embedding model we use: https://www.nomic.ai/blog/posts/nomic-embed-text-v1
+        //        "nomic-embed-text" is a large context length text encoder that surpasses
+        //        OpenAI "text-embedding-ada-002" and "text-embedding-3-small"
+        //        performance on short and long context tasks.
+        //TODO: Maybe later we will use a 1b or 3b embedding model to save some RAM and CPU/GPU.
+        // Check ollama
         String jsonBody = null;
-        try (HttpClient client = HttpClient.newHttpClient()) {
+        try (final HttpClient client = HttpClient.newHttpClient()) {
 
             final JSONObject ollamaPayload = new JSONObject();
             ollamaPayload.put("model", "nomic-embed-text");
@@ -77,7 +81,8 @@ public class Uploader {
 
         LOGGER.info(jsonBody);
 
-        //json parsing of the embedding
+        // Step 3:
+        // parsing the JSON of the embedding
         final JSONObject data = JSON.parseObject(jsonBody);
         if (isNull(data) || !data.containsKey("embedding")) {
             LOGGER.severe("no embedding found");
@@ -86,6 +91,44 @@ public class Uploader {
         final JSONArray embedding = data.getJSONArray("embedding");
         final List<Float> embeddingList = embedding.toList(Float.class);
         LOGGER.info("embeddingList size: " + embeddingList.size());
+
+        // Step 4:
+        // build up JSON Payload for the next step
+        final JSONObject vectorPayload = new JSONObject();
+
+        final JSONObject point = new JSONObject();
+        point.put("id", UUID.randomUUID());
+        final JSONObject pointPayload = new JSONObject();
+        pointPayload.put("filename", fileName);
+        point.put("payload", pointPayload);
+        point.put("vector", embeddingList);
+
+        final JSONArray array = new JSONArray();
+        array.add(point);
+        vectorPayload.put("points", array);
+
+        // Step 5:
+        // upload the payload into a vector DB
+        // We use as vector DB Qdrant
+        // more infos: https://qdrant.tech/
+        //TODO: Maybe later we will change the vector DB. Check for state of sqlite-vec
+        try (final HttpClient client = HttpClient.newHttpClient()) {
+               final HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(isNull(VECTOR_URL) ? "http://localhost:6333/collections/test_collection/points?wait=true" : VECTOR_URL))
+                    .PUT(HttpRequest.BodyPublishers.ofString(vectorPayload.toJSONString()))
+                    .setHeader("Content-Type", "application/json")
+                    .build();
+
+            final HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            if (response.body().contains("acknowledged")) {
+                LOGGER.info("upload successful");
+            } else {
+                LOGGER.severe("Some Problems with the upload of the embedding into the vector DB");
+            }
+
+        } catch (IOException | InterruptedException ex) {
+            LOGGER.severe("Exception on upload to the vector DB" + ex.getLocalizedMessage());
+        }
 
     }
 
